937ea8a46c078b863e2643224a578551cd761011
qiuqfang <qiuqfang@outlook.com>
2023-06-27 12:51:03 +0800

    update

--- scrapy_app/spiders/bqg2.py
+++ scrapy_app/spiders/bqg2.py
@@ -4,8 +4,8 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy_app.items import Bqg2Item
 
 
-class Bqg2_1Spider(scrapy.Spider):
-    name = "bqg2_1"
+class Bqg2Spider(scrapy.Spider):
+    name = "bqg2"
     allowed_domains = ["www.bqg2.org"]
     start_urls = ["https://www.bqg2.org/212/212267/"]
 
@@ -15,24 +15,22 @@ class Bqg2_1Spider(scrapy.Spider):
     page = 1
     a_list = None
     a_list_len = 0
-    current_a = 2400
+    current_a = 20
     fp = None
+    all_page = None
 
     def parse(self, response):
         self.a_list = response.xpath("//div[@class='ml_list']//li/a")
         self.a_list_len = len(self.a_list)
         print(self.a_list_len)
-
-        href = self.a_list[self.current_a].xpath("./@href").extract_first()
-        self.base_href = href[0:len(href) - 5]
-        self.page = 1
-        url = self.base_url + self.base_href + "_" + str(self.page) + ".html"
-        self.fp = open("./bqg2/book.csv", 'w', encoding='utf-8')
-        self.fp.write(str(self.current_a + 1) + ",")
+        url = self.common_fn("w")
         yield scrapy.Request(url, callback=self.other_parse)
-        pass
 
     def other_parse(self, response):
+        if self.all_page is None:
+            title = str.strip(response.xpath("//div[@class='nr_title']//h3/text()").extract_first())
+            self.all_page = int(title[len(title) - 2])
+
         content_list = response.xpath("//p[@class='articlecontent']/text()").extract()
 
         page_content = ""
@@ -43,7 +41,7 @@ class Bqg2_1Spider(scrapy.Spider):
         bqg2 = Bqg2Item(content=page_content, fp=self.fp)
         yield bqg2
 
-        if self.page < 3:
+        if self.page < self.all_page:
             self.page += 1
             url = self.base_url + self.base_href + "_" + str(self.page) + '.html'
             yield scrapy.Request(url, callback=self.other_parse)
@@ -51,11 +49,20 @@ class Bqg2_1Spider(scrapy.Spider):
             self.fp.write("\n")
             self.fp.close()
             if self.current_a < self.a_list_len - 1:
+                self.all_page = None
                 self.current_a += 1
-                self.fp = open("./bqg2/book.csv", "a", encoding="utf-8")
-                self.fp.write(str(self.current_a + 1) + ",")
-                href = self.a_list[self.current_a].xpath("./@href").extract_first()
-                self.base_href = href[0:len(href) - 5]
-                self.page = 1
-                url = self.base_url + self.base_href + "_" + str(self.page) + ".html"
+                url = self.common_fn("a")
                 yield scrapy.Request(url, callback=self.other_parse)
+
+    def common_fn(self, fp_type):
+
+        self.fp = open("./bqg2/book.csv", fp_type, encoding="utf-8")
+        if fp_type == 'w':
+            self.fp.write("章节,内容\n")
+            
+        self.fp.write(str(self.current_a + 1) + ",")
+        href = self.a_list[self.current_a].xpath("./@href").extract_first()
+        self.base_href = href[0:len(href) - 5]
+        self.page = 1
+        url = self.base_url + self.base_href + "_" + str(self.page) + ".html"
+        return url
